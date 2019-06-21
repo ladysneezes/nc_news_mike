ad4f0860794798e4e425f245fc2a044b13d36a94
ladysneezes <franktmike@gmail.com>
2019-06-21 10:38:46 +0100

    added heroku hosting info

--- db/connection.js
+++ db/connection.js
@@ -1,3 +1,13 @@
-const dbConfig = require('../knexfile');
+// const dbConfig = require('../knexfile');
 
-module.exports = require('knex')(dbConfig);
+// module.exports = require('knex')(dbConfig);
+
+const ENV = process.env.NODE_ENV || "development";
+const knex = require("knex");
+
+const dbConfig =
+  ENV === "production"
+    ? { client: "pg", connection: process.env.DATABASE_URL }
+    : require("../knexfile");
+
+module.exports = knex(dbConfig);
